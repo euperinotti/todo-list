ed504825bf2dcfd3120450ff41b4007e078cb774
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-16 22:19:10 -0300

    feat: add handle open task info method

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Task } from "../../components/Task";
+import { TaskInfo } from "../../patterns/TaskInfo";
 import { ITask } from "../../types/ITask";
 import { TaskStatus } from "../../types/TaskStatus";
 
 const exampleTask: ITask = {
+  id: "1",
   name: "Task",
-  dateAdded: "",
+  dateAdded: "2024-12-06 00:00:00",
   dateModified: "",
   status: TaskStatus.ACTIVE,
   dateCompleted: "",
   dueDate: "",
-  notes: ""
+  notes: "ajsdasdaskdl"
 }
 
 export const Home = () => {
+
+  const [selectedTask, setSelectedTask] = useState<ITask | null>()
+
+  const handleOpenTaskInfo = (task: ITask) => {
+    setSelectedTask(task)
+  }
+
   return (
     <div className="bg-slate-900 h-screen text-slate-50 py-32 px-60">
-      <div className="flex justify-start items-start h-full w-full">
+      <div className="flex justify-start items-start h-full w-full gap-12">
         <div className="flex flex-col justify-start items-start h-auto w-full gap-6 max-w-lg">
           <Header />
           <div className="flex w-full gap-3">
@@ -27,7 +37,13 @@ export const Home = () => {
           </div>
 
           <section className="max-h-72 w-full">
-            <Task data={exampleTask} />
+            <Task data={exampleTask} onClick={() => handleOpenTaskInfo(exampleTask)} />
+          </section>
+        </div>
+
+        <div className="flex flex-col justify-start items-start h-auto w-full gap-6 max-w-lg">
+          <section className="max-h-72 w-full">
+            {selectedTask && (<TaskInfo data={selectedTask} />)}
           </section>
         </div>
       </div>
