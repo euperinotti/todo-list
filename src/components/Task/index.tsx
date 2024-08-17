24a788c5283f7472b0024713152682e787517a20
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-16 22:18:53 -0300

    refactor: add more info to task component

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -7,7 +7,17 @@ export const Task = ({ data, ...rest }: TaskProps) => {
       className="w-full flex justify-between items-center bg-slate-800 border rounded border-slate-700 p-3 hover:bg-slate-700"
       {...rest}
     >
-      <h3 className="font-bold text-xl">{data.name}</h3>
+      <div className="w-full">
+        <h3 className="font-bold text-xl">{data.name}</h3>
+        <div className="w-full flex items-center justify-start gap-2 text-slate-500 text-sm">
+          <span>{data.dateAdded}</span>
+          {data.notes && (
+            <>
+              <p>•</p> <span>=</span>
+            </>
+          )}
+        </div>
+      </div>
       <CheckTask />
     </div>
   );
