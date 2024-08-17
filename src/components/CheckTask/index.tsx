10a90b1caa86e6162f55fe16be5557681d0e1763
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-16 22:18:32 -0300

    styles: changed checktask styles

--- src/components/CheckTask/index.tsx
+++ src/components/CheckTask/index.tsx
@@ -1,5 +1,32 @@
-import { CheckTaskProps } from "./types"
+import { CheckTaskProps } from "./types";
 
 export const CheckTask = ({}: CheckTaskProps) => {
-  return (<input type="checkbox" className="appearance-none text-red-500 red-500 border border-slate-700 rounded-full w-7 h-7"/>)
-}
+  return (
+    <label
+      className="relative flex items-center rounded-full cursor-pointer"
+      htmlFor="customStyle"
+    >
+      <input
+        type="checkbox"
+        className="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-full border border-sky-700/20 bg-sky-700/10 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-green-400 checked:bg-green-600 checked:before:bg-green-900 hover:scale-105 hover:before:opacity-0"
+        id="customStyle"
+      />
+      <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-3.5 w-3.5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          stroke="currentColor"
+          stroke-width="1"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </span>
+    </label>
+  );
+};
