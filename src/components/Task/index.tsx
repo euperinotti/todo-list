faad6878bf6ae7bce442376ac6c2cda69ac5c909
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-22 21:06:27 -0300

    refactor: component layout and date rendering

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,16 +4,15 @@ import { TaskProps } from "./types";
 export const Task = ({ data, ...rest }: TaskProps) => {
   return (
     <div
-      className="w-full flex justify-between items-center bg-slate-800 border rounded border-slate-700 p-3 hover:bg-slate-700"
+      className="w-full h-auto flex justify-between items-center bg-slate-800 border rounded border-slate-700 p-3 hover:bg-slate-700"
       {...rest}
     >
       <div className="w-full">
         <h3 className="font-bold text-xl">{data.name}</h3>
         <div className="w-full flex items-center justify-start gap-2 text-slate-500 text-sm">
-          <span>{data.dateAdded}</span>
+          <span>{data.dateAdded.toLocaleDateString("pt-BR")}</span>
           {data.notes && (
             <>
-              {/* <p>•</p> <Icon color="text-slate-500" name="smallNote" /> */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
