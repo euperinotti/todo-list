3bddd15c36d8a215c7f7b133786da289238483e5
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-22 23:53:02 -0300

    feat: add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,50 +1,38 @@
-import { useState } from "react";
-import { Button } from "../../components/Button";
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
-import { Task } from "../../components/Task";
-import { TaskInfo } from "../../patterns/TaskInfo";
-import { ITask } from "../../types/ITask";
-import { TaskStatus } from "../../types/TaskStatus";
-
-const exampleTask: ITask = {
-  id: "1",
-  name: "Task",
-  dateAdded: "2024-12-06 00:00:00",
-  dateModified: "",
-  status: TaskStatus.ACTIVE,
-  dateCompleted: "",
-  dueDate: "",
-  notes: "ajsdasdaskdl"
-}
-
-export const Home = () => {
-
-  const [selectedTask, setSelectedTask] = useState<ITask | null>()
-
-  const handleOpenTaskInfo = (task: ITask) => {
-    setSelectedTask(task)
-  }
-
+export const HomePage = () => {
   return (
-    <div className="bg-slate-900 h-screen text-slate-50 py-32 px-60">
-      <div className="flex justify-start items-start h-full w-full gap-12">
+    <div className="bg-slate-900 h-screen text-slate-50 py-12 px-60">
+      <div className="flex flex-col justify-start items-start h-full w-full gap-12">
+        <nav className="text-slate-500 flex gap-4 items-center justify-center">
+          <a href="/" className="hover:text-sky-500">
+            Home
+          </a>
+          <a href="/about" className="hover:text-sky-500">
+            About
+          </a>
+          <a href="/todo" className="p-2 bg-sky-600 text-slate-100 rounded">
+            Todo App
+          </a>
+        </nav>
         <div className="flex flex-col justify-start items-start h-auto w-full gap-6 max-w-lg">
-          <Header />
-          <div className="flex w-full gap-3">
-            <Input className="flex-1" />
-            <Button label="Add to list" />
-          </div>
-
-          <section className="max-h-72 w-full">
-            <Task data={exampleTask} onClick={() => handleOpenTaskInfo(exampleTask)} />
-          </section>
-        </div>
-
-        <div className="flex flex-col justify-start items-start h-auto w-full gap-6 max-w-lg">
-          <section className="max-h-72 w-full">
-            {selectedTask && (<TaskInfo data={selectedTask} />)}
-          </section>
+          <h1 className="text-5xl font-bold text-slate-100">
+            What is a To do app?
+          </h1>
+          <p className="text-slate-500 text-lg">
+            What is a ToDo List? The definition is a simple one. It’s a list of
+            tasks you need to complete or things that you want to do. Most
+            typically, they’re organised in order of priority. Traditionally,
+            they’re written on a piece of paper or post it notes and act as a
+            memory aid. As technology has evolved we have been able to create a
+            todo lists with excel spreadsheets, word documents, email lists,
+            todo list apps, Microsoft to do and google to do list to name a few.
+            You can use a to do list in your home and personal life, or in the
+            workplace. Having a list of everything you need to do written down
+            in one place means you shouldn’t forget anything important. By
+            prioritising the tasks in the list you plan the order in which
+            you’re going to do them and can quickly see what needs your
+            immediate attention and what tasks you can leave until a little
+            later.
+          </p>
         </div>
       </div>
     </div>
