261352eb0963ae26641d2ac55af5adc221db53dd
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-22 23:54:12 -0300

    feat: updating task

--- src/patterns/TaskInfo/index.tsx
+++ src/patterns/TaskInfo/index.tsx
@@ -3,18 +3,19 @@ import { Button } from "../../components/Button";
 import { ITask } from "../../types/ITask";
 import { TaskInfoProps } from "./types";
 
-export const TaskInfo = ({ data }: TaskInfoProps) => {
+export const TaskInfo = ({ data, config }: TaskInfoProps) => {
   const [newTaskInfo, setNewTaskInfo] = useState<ITask>(data);
 
   const saveChanges = () => {
-    console.log(newTaskInfo);
+    config.setSelectedTask({...data, ...newTaskInfo});
+    setNewTaskInfo(data);
   };
 
   return (
     <div className="p-6 flex flex-col w-100 bg-slate-800 border border-slate-700 rounded-md gap-2">
-      <h1 className="text-4xl font-bold">{data.name}</h1>
+      <h1 className="text-4xl font-bold">{newTaskInfo.name}</h1>
       <p className="text-lg text-slate-500">
-        {data.dateAdded.toLocaleDateString("pt-BR")} -{" "}
+        {newTaskInfo.dateAdded.toLocaleDateString("pt-BR")} -{" "}
         <input
           type="datetime-local"
           value={newTaskInfo?.dueDate}
