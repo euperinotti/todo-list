2a8b2a59efa34d24f4a48fa85aca6d7bb0bd8f97
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-22 23:53:28 -0300

    refactor: add routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import "./App.css";
-import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/Home";
+import { TodoPage } from "./pages/Todo";
+import { AboutPage } from "./pages/About";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
