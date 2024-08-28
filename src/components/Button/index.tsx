aa287bee3aa427d0c8c79d89304ff6d59368c7cb
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-27 23:04:19 -0300

    fix: button styles not working

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,18 @@
-import { ButtonProps, ButtonVariant } from "./types";
+import { ButtonProps } from "./types";
+
+export const ButtonVariant = {
+  default:
+    "px-3 py-2 bg-sky-700 hover:bg-sky-600 border rounded border-sky-600 font-medium w-fit transition",
+  danger:
+    "px-3 py-2 bg-red-800 hover:bg-red-700 border rounded border-red-700 font-medium w-fit transition",
+};
 
 export const Button = ({ label, variant, ...rest }: ButtonProps) => {
+
+  let buttonStyles = ButtonVariant[variant];
+
   return (
-    <button
-      className={ButtonVariant[variant] || ButtonVariant.default}
-      {...rest}
-    >
+    <button className={`${buttonStyles}`} {...rest}>
       {label}
     </button>
   );
