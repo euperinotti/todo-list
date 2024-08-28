b85017d15177c5fdfc6a123731daf9c949408783
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-28 13:53:33 -0300

    refactor: code refactor

--- src/template/Base/index.tsx
+++ src/template/Base/index.tsx
@@ -4,7 +4,7 @@ import { BaseTemplateProps } from "./types";
 export const BaseTemplate = ({ children, ...rest }: BaseTemplateProps) => {
   return (
     <div
-      className={`bg-slate-900 min-h-screen h-full text-slate-50 py-12 px-60 ${rest.className}`}
+      className={`bg-slate-900 min-h-dvh h-full text-slate-50 py-12 px-60 ${rest.className}`}
       {...rest}
     >
       <div className="flex flex-col justify-start items-start h-full w-full gap-12">
